a4404281f1f6dfa33f21f13ad489934e37319a35
LevKhaak <leu.khaak@gmail.com>
2023-01-20 17:51:13 +0300

    portfolio.js

---
+++ portfolio.js
@@ -0,0 +1,34 @@
+1. Breaking Bad - приложение поиска киногероев. Использованы технологии: атомарный дизайн, scss, React + redux, middleware Redux Thunk, fetch-запросы,  unit- тесты и e2e- тесты, Docker.
+Сайт: https://leukhaak.github.io/breaking-bad-new/#/   
+
+докерхаб-репозиторий: https://hub.docker.com/r/levkhaak1968/breackingbad 
+
+гитхаб-pепозиторий: https://github.com/LeuKhaak/breaking-bad-new 
+
+2. Weather-app – погодный информер. Использованы технологии: атомарный дизайн, scss, React + Typescript, fetch-запросы, e2e- тесты, Docker.
+Презентация в слайдере: https://leukhaak.github.io/presentation-w-a/ 
+
+докерхаб-репозиторий: https://hub.docker.com/r/levkhaak1968/weatherapp 
+ 
+гитхаб-репозиторий: https://github.com/LeuKhaak/weather-app-1 
+
+3. ToDo-list - Использованы технологии: scss, React, сохранение дефолтных данных в в json- файле, размещение на netlify:
+Сайт: https://test-ivashin-notes.netlify.app/   
+
+репозиторий: https://github.com/LeuKhaak/test-notes   
+
+4. ToDo-list - Использованы технологии: атомарный дизайн, scss, vue + vuex, unit- тесты и e2e- тесты.
+репозиторий: https://github.com/LeuKhaak/vue_1  
+
+5. Сайт интернет-магазина -  Использованы технологии: React (классовые компоненты), запросы graphQl, Docker.
+Презентация в слайдере: https://leukhaak.github.io/presentation-s-t/  
+
+гитхаб-репозиторий: https://github.com/LeuKhaak/Scandiweb-test   
+
+докерхаб-репозиторий: https://hub.docker.com/r/levkhaak1968/scandiwebtest 
+
+бэкенд graphQl (скопирован с репозитория разработчика в готовом виде)  
+
+гитхаб-репозиторий: https://github.com/LeuKhaak/Scandiweb-test-graphQl 
+
+докерхаб-репозиторий: https://hub.docker.com/r/levkhaak1968/graphqlbackend  
