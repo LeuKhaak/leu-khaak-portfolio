b657376aaab8518b05da737700cb0b1809a832a2
LevKhaak <leu.khaak@gmail.com>
2023-02-17 12:46:57 +0300

    files updated 17.02

--- portfolio.js
+++ portfolio.js
@@ -1,21 +1,21 @@
-1. Breaking Bad - приложение поиска киногероев. Использованы технологии: атомарный дизайн, scss, React + redux, middleware Redux Thunk, fetch-запросы,  unit- тесты и e2e- тесты, Docker.
-Сайт: https://leukhaak.github.io/breaking-bad-new/#/   
-
-докерхаб-репозиторий: https://hub.docker.com/r/levkhaak1968/breackingbad 
-
-гитхаб-pепозиторий: https://github.com/LeuKhaak/breaking-bad-new 
-
-2. Weather-app – погодный информер. Использованы технологии: атомарный дизайн, scss, React + Typescript, fetch-запросы, e2e- тесты, Docker.
+1. Weather-app – погодный информер. Использованы технологии: атомарный дизайн, scss, React + Typescript, fetch-запросы, e2e- тесты, Docker.
 Презентация в слайдере: https://leukhaak.github.io/presentation-w-a/ 
 
 докерхаб-репозиторий: https://hub.docker.com/r/levkhaak1968/weatherapp 
  
 гитхаб-репозиторий: https://github.com/LeuKhaak/weather-app-1 
 
-3. ToDo-list - Использованы технологии: scss, React, сохранение дефолтных данных в в json- файле, размещение на netlify:
+2. ToDo-list - Использованы технологии: scss, React, сохранение дефолтных данных в в json- файле, размещение на netlify:
 Сайт: https://test-ivashin-notes.netlify.app/   
 
-репозиторий: https://github.com/LeuKhaak/test-notes   
+репозиторий: https://github.com/LeuKhaak/test-notes
+
+3. Breaking Bad - приложение поиска киногероев. Использованы технологии: атомарный дизайн, scss, React + redux, middleware Redux Thunk, fetch-запросы,  unit- тесты и e2e- тесты, Docker.
+Сайт: https://leukhaak.github.io/breaking-bad-new/#/ - к сожалению, в настоящее время доступ к api сайта www.breakingbadapi.com утрачен и запросы не работают :( 
+
+докерхаб-репозиторий: https://hub.docker.com/r/levkhaak1968/breackingbad 
+
+гитхаб-pепозиторий: https://github.com/LeuKhaak/breaking-bad-new   
 
 4. ToDo-list - Использованы технологии: атомарный дизайн, scss, vue + vuex, unit- тесты и e2e- тесты.
 репозиторий: https://github.com/LeuKhaak/vue_1  
